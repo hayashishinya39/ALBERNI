ecce08abae0bfe71df4d4a6a67567c91c663b603
EC2 Default User <ec2-user@ip-172-31-3-206.ec2.internal>
2019-02-14 05:54:18 +0000

    Innitial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ALBERNIへようこそ</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <!-- ヘッダー始まり -->
+      <header class="header">
+        <h1 class="logo"><a href="index.html"><img src="images/logo1.jpg" alt=""></a></h1>
+        <nav class="nav">
+          <ul>
+            <li><a href="index.html">ホーム</a></li>
+            <li><a href="greet.html">店主</a></li>
+            <li><a href="about.html">店舗案内</a></li>
+            <li><a href="access.html">アクセス</a></li>
+            <li><a href="menu.html">メニュー</a></li>
+            <li><a href="fuukei.html">店舗周辺</a></li>
+            <li><a href="contact.html">お問い合わせ</a></li>
+          </ul>
+        </nav>
+      </header>
+      <!--　ヘッダー終了 -->
+      <!-- メイン始まり　-->
+      <div class="keyvisual">
+        <img src="images/keyvisual.jpg" alt="">
+      </div>
+      <main>
+        <h2 id="news">News</h2>
+        <p class="news-item">2月は明治大学や、慶応大学にも出店いたします。　出店予定地や日程などは随時配信いたしますので、ご確認ください</p>
+        <p class="news-item">2月20日は文化祭「<a href="http://www.sbcr.jp" target="_blank">PICNIC</a>」のライブを開催します。投げ銭方式です。お楽しみに！</p>
+      </main>
+      <!--　メイン終わり -->
+      <!--　フッダー始まり -->
+      <footer class="footer">
+        <p>&copy;Copyright ALBERNI. All rights reserved.</p>
+      </footer>
+      <!--　フッダー終わり  -->
+    </div>
+  </body>
+</html>
